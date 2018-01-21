adc0c6f5ba0d60bb1a86bb4cae9c4b3447a81dc9
Jojo <jojo@pc.com>
2018-01-21 22:21:55 +0800

    Initial commit

---
+++ app/src/main/java/dev/jojo/seismonitor/SeismoSplash.java
@@ -0,0 +1,15 @@
+package dev.jojo.seismonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeismoSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seismo_splash);
+
+
+    }
+}
